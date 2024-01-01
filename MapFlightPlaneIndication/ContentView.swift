ee7e4c58246be61a132c053f621c93d5395d177a
Harry-KNIGHT <eliott.knight@icloud.com>
2024-01-01 21:37:57 +0100

    feat: add annotation view on map

--- MapFlightPlaneIndication/ContentView.swift
+++ MapFlightPlaneIndication/ContentView.swift
@@ -12,7 +12,13 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             ZStack(alignment: .bottom) {
-                Map()
+                Map {
+                    Annotation("Orly airport", coordinate: .orlyAirport) {
+                        Image(systemName: "airplane")
+                            .font(.system(size: 40))
+                    }
+                    .annotationTitles(.hidden)
+                }
 
                 FlightInformationsView()
                 .padding()
@@ -21,6 +27,11 @@ struct ContentView: View {
         }
         .ignoresSafeArea()
     }
+}
+extension CLLocationCoordinate2D {
+    static let orlyAirport = CLLocationCoordinate2D(latitude: 48.7262433, longitude: 2.365247199999999)
+    static let empireStateBuilding = CLLocationCoordinate2D(latitude: 40.7484, longitude: -73.9857)
+    static let columbiaUniversity = CLLocationCoordinate2D(latitude: 40.8075, longitude: -73.9626)
 }
 
 #Preview {
