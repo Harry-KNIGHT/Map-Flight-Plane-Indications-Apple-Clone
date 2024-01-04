2f0b80b53aa458ee1b214350ca110755e74a683f
Harry-KNIGHT <eliott.knight@icloud.com>
2024-01-04 21:42:50 +0100

    feat: conform CLL2D to Hashableand add pickers

--- MapFlightPlaneIndication/ContentView.swift
+++ MapFlightPlaneIndication/ContentView.swift
@@ -9,11 +9,13 @@ import SwiftUI
 import MapKit
 
 struct ContentView: View {
+    @State private var departureAirport: CLLocationCoordinate2D = .orlyAirport
+    @State private var arrivalAirport: CLLocationCoordinate2D = .newarkAirport
     var body: some View {
         NavigationStack {
             VStack(alignment: .center, spacing: 0) {
                 Map {
-                    Annotation("Orly airport", coordinate: .orlyAirport) {
+                    Annotation("Orly airport", coordinate: departureAirport) {
                         Image(systemName: "airplane")
                             .font(.system(size: 20))
                             .rotationEffect(.degrees(200))
@@ -23,9 +25,23 @@ struct ContentView: View {
                     MapPolyline(coordinates: CLLocationCoordinate2D.airports, contourStyle: .geodesic)
                         .stroke(Color.orange, lineWidth: 2.5)
                 }
-                FlightInformationsView()
-                    .padding()
-                    .background(.thinMaterial)
+
+                HStack {
+                    Picker("Airport departure", selection: $departureAirport) {
+                        Text("Orly")
+                            .tag(CLLocationCoordinate2D.orlyAirport)
+                        Text("Newark")
+                            .tag(CLLocationCoordinate2D.newarkAirport)
+                    }
+                    Spacer()
+                    Picker("Airport arrival", selection: $arrivalAirport) {
+                        Text("Orly")
+                            .tag(CLLocationCoordinate2D.orlyAirport)
+                        Text("Newark")
+                            .tag(CLLocationCoordinate2D.newarkAirport)
+                    }
+                }
+                .padding()
             }
         }
     }
--- MapFlightPlaneIndication/Extension+CLLocation2D.swift
+++ MapFlightPlaneIndication/Extension+CLLocation2D.swift
@@ -14,3 +14,14 @@ extension CLLocationCoordinate2D {
 
     static let airports: [CLLocationCoordinate2D] = [.orlyAirport, newarkAirport]
 }
+
+extension CLLocationCoordinate2D: Hashable {
+    public static func == (lhs: Self, rhs: Self) -> Bool {
+        return lhs.latitude == rhs.latitude && lhs.longitude == rhs.longitude
+    }
+
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(latitude)
+        hasher.combine(longitude)
+    }
+}
