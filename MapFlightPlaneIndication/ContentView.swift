ae5174050f45c9bf4893851eaa0d7b6972d9a0cf
Harry-KNIGHT <eliott.knight@icloud.com>
2024-01-04 21:33:12 +0100

    feat: remove unuseful code

--- MapFlightPlaneIndication/ContentView.swift
+++ MapFlightPlaneIndication/ContentView.swift
@@ -9,20 +9,10 @@ import SwiftUI
 import MapKit
 
 struct ContentView: View {
-    @State private var cameraPosition: MapCameraPosition = .camera(
-        MapCamera(
-            centerCoordinate: .init(latitude: 22.176310, longitude:-41.176251),
-            distance: .infinity
-            )
-    )
-
-
     var body: some View {
         NavigationStack {
             VStack(alignment: .center, spacing: 0) {
-                Map(
-                    position: $cameraPosition
-                ) {
+                Map {
                     Annotation("Orly airport", coordinate: .orlyAirport) {
                         Image(systemName: "airplane")
                             .font(.system(size: 20))
@@ -38,30 +28,6 @@ struct ContentView: View {
                     .background(.thinMaterial)
             }
         }
-        .onAppear {
-            averageLocationBetweenTwoAirports()
-        }
-    }
-
-    private func averageLocationBetweenTwoAirports() {
-        let centerCoordinate = calculAverageLocationBetweenTwoAirports()
-
-        cameraPosition = .camera(
-            MapCamera(
-                centerCoordinate: centerCoordinate,
-                distance: 19059990
-            )
-        )
-    }
-
-    private func calculAverageLocationBetweenTwoAirports() -> CLLocationCoordinate2D {
-        let orlyAirport = CLLocationCoordinate2D.orlyAirport
-        let newarkAirport = CLLocationCoordinate2D.newarkAirport
-
-        let avgLatitude = (orlyAirport.latitude + newarkAirport.latitude) / 2
-        let avgLongitude = (orlyAirport.longitude + newarkAirport.longitude) / 2
-
-        return CLLocationCoordinate2D(latitude: avgLatitude, longitude: avgLongitude)
     }
 }
 
